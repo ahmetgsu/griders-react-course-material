f79a1e3fba3a7f144486df2dcf4fc9e065e01720
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-13 19:32:08 +0300

    301-311-implementing and rendering the language store

--- section-23-context-system/translate/src/components/App.js
+++ section-23-context-system/translate/src/components/App.js
@@ -1,27 +1,20 @@
 import React, { Component } from "react";
 import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
+import { LanguageStore } from "../contexts/LanguageContext";
 import ColorContext from "../contexts/ColorContext";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends Component {
-  state = { language: "english" };
-
-  onLanguageChange = language => {
-    console.log(language);
-    this.setState({ language: language });
-  };
-
   render() {
     console.log(this.state);
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <ColorContext.Provider value="green">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="green">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
