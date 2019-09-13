469fba2dc7a90986ced1178f71929495244fb247
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-13 17:17:10 +0300

    305-gotchas around providers

--- section-23-context-system/translate/src/contexts/LanguageContext.js
+++ section-23-context-system/translate/src/contexts/LanguageContext.js
@@ -1,3 +1,11 @@
 import React from "react";
 
 export default React.createContext("english"); // default value is english
+
+// import React from "react";
+
+// const context = React.createContext("english");
+
+// console.log(context);
+
+// export default context;
