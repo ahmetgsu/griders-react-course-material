f59aaf04ef6a493068e4559b2aabcc26db16ebfe
ahmetgsu <ahmet.gsu@gmail.com>
2019-09-13 17:33:57 +0300

    306-accessing data with consumer

--- section-23-context-system/translate/src/components/Button.js
+++ section-23-context-system/translate/src/components/Button.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
-  // 1st thing is to set up contextType
-  // this is how we hook up context object to related Component
-  // contextType name is special for this purpose. DO  NOT USE ANOTHER NAME INSTEAD
-  static contextType = LanguageContext;
+  // Getting data by using "CONSUMER"
+  // When using Consumer, we dont need to contextType
 
   render() {
     console.log(this.context);
-    const text = this.context === "english" ? "Submit" : "VoorLeggen";
-    return <button className="ui button primary">{text}</button>;
+
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {value => (value === "english" ? "Submit" : "Voorleggen")}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
@@ -18,3 +22,22 @@ class Button extends Component {
 // Button.contextType = LanguageContext;
 
 export default Button;
+
+// this.context type of getting data from Context Object
+// class Button extends Component {
+//     // 1st thing is to set up contextType
+//     // this is how we hook up context object to related Component
+//     // contextType name is special for this purpose. DO  NOT USE ANOTHER NAME INSTEAD
+//     static contextType = LanguageContext;
+
+//     render() {
+//       console.log(this.context);
+//       const text = this.context === "english" ? "Submit" : "VoorLeggen";
+//       return <button className="ui button primary">{text}</button>;
+//     }
+//   }
+
+//   // The same meaning with static, but outside of class
+//   // Button.contextType = LanguageContext;
+
+//   export default Button;
